4fe9239c726d0b4916ba23320985d563b1c3ec6a
Murilo Barros <muribeco@hotmail.com>
2025-01-29 21:11:27 +0000

    Funções Alteradas com Paginas E Murilo botando terror

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
-function Home() {
-  return <h1>Da um sorriso para mim ;O </h1> 
+// Função para fechar o aviso
+function fecharAviso() {
+  const aviso = document.getElementById('aviso');
+  if (aviso) aviso.style.display = 'none';
+
+  // Exibir o pop-up após o fechamento do aviso
+  const popup = document.getElementById('popup');
+  if (popup) popup.style.display = 'flex';
 }
 
-export default Home;
+// Adiciona o evento de clique para fechar o aviso
+const fecharBtn = document.getElementById('fecharBtn');
+if (fecharBtn) {
+  fecharBtn.addEventListener('click', fecharAviso);
+}
+
+// Função para fechar o pop-up
+const popupBtn = document.getElementById('popupBtn');
+if (popupBtn) {
+  popupBtn.addEventListener('clik', function() {
+      const popup = document.getElementById('popup');
+      if (popup) popup.style.display = 'none';
+  });
+}
