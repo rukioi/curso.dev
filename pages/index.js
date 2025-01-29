7c4de1284f87d4f3c3a825b11d714d0dac287cb2
Murilo Barros <muribeco@hotmail.com>
2025-01-29 21:19:32 +0000

    Mudança Cliente

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,31 @@
-// Função para fechar o aviso
-function fecharAviso() {
-  const aviso = document.getElementById('aviso');
-  if (aviso) aviso.style.display = 'none';
+if (typeof window !== "undefined") {
+  // Função para fechar o aviso
+  function fecharAviso() {
+    const aviso = document.getElementById('aviso');
+    if (aviso) aviso.style.display = 'none';
 
-  // Exibir o pop-up após o fechamento do aviso
-  const popup = document.getElementById('popup');
-  if (popup) popup.style.display = 'flex';
-}
+    // Exibir o pop-up após o fechamento do aviso
+    const popup = document.getElementById('popup');
+    if (popup) popup.style.display = 'flex';
+  }
 
-// Adiciona o evento de clique para fechar o aviso
-const fecharBtn = document.getElementById('fecharBtn');
-if (fecharBtn) {
-  fecharBtn.addEventListener('click', fecharAviso);
-}
+  // Função para fechar o pop-up
+  function fecharPopup() {
+    const popup = document.getElementById('popup');
+    if (popup) popup.style.display = 'none';
+  }
 
-// Função para fechar o pop-up
-const popupBtn = document.getElementById('popupBtn');
-if (popupBtn) {
-  popupBtn.addEventListener('clik', function() {
-      const popup = document.getElementById('popup');
-      if (popup) popup.style.display = 'none';
+  // Adiciona o evento de clique para fechar o aviso
+  document.addEventListener('DOMContentLoaded', function() {
+    const fecharBtn = document.getElementById('fecharBtn');
+    if (fecharBtn) {
+      fecharBtn.addEventListener('click', fecharAviso);
+    }
+
+    // Adiciona o evento de clique para fechar o pop-up
+    const popupBtn = document.getElementById('popupBtn');
+    if (popupBtn) {
+      popupBtn.addEventListener('click', fecharPopup);
+    }
   });
 }
